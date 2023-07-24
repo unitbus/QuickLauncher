2ccc8eceb2a73090978ef61cea08398174cf208a
hoshina <co.unitbus@gmail.com>
2023-07-24 13:32:12 +0900

    remove comments.

--- samples/sample.bat
+++ samples/sample.bat
@@ -2,10 +2,6 @@
 cd /D %~dp0..\
 echo %CD%
 
-:: 通常は、EXEと同じ場所にあるQuickLauncher.jsonが設定ファイルとなります
 start "QL-A" ".\release\QuickLauncher.exe"
-
-:: 複数起動する事が可能で、設定ファイルの場所や、アイコンも変更可能です
 start "QL-B" ".\release\QuickLauncher.exe" -json ".\samples\sample.json" -icon ".\samples\sample.ico"
-
 pause
